890dd6455dd2b53b6f8b6e49fb018c5241d03d82
Mariana <m.montoyat@uniandes.edu.co>
2023-09-05 22:26:07 -0500

    Parte 1

--- Taller_1 - Parte 1/Indices_refraccion.py
+++ Taller_1 - Parte 1/Indices_refraccion.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 def tuplas_longitud_onda(archivo):
     arreglo = []
@@ -20,23 +22,85 @@ def tuplas_longitud_onda(archivo):
     return arreglo
 
 tuplas_plastico = tuplas_longitud_onda('./Plásticos/French.yml')
-#tuplas_plastico = tuplas_longitud_onda('./Combustible/Wang-20C.yml')
+tuplas_plastico = tuplas_longitud_onda('./Combustible/Wang-20C.yml')
+
+
+
+
+#Promedio n
+
+def promedio(n:list):
+    resultado = 0
+
+    for promedio in n:
+        resultado += promedio[1]
+    resultado = resultado/len(n)
+    return resultado
+
+promedio_n = promedio(tuplas_plastico)
+
+#Desviacion estandar de muestra
+def desviacion_estandar(n:list, p):
+
+    suma = 0
+    for i in range(len(n)):
+        resta = (n[i][1] - p)**2
+        suma += resta
+    division = suma/(len(n) - 1)
+    raiz = division**(1/2)
+    return raiz
+
+desviacion_plastico = desviacion_estandar(tuplas_plastico, promedio_n)
+
 
 #Grafica indice de refraccion
 fig,ax = plt.subplots()
 ax.set_title('Kapton')
+rango = np.linspace(min([tuplas[0] for tuplas in tuplas_plastico]),max([tuplas[0] for tuplas in tuplas_plastico]),100)
+ax.plot(rango,np.full_like(rango,promedio_n),color='r',label = 'promedio')
+ax.plot(rango,np.full_like(rango,promedio_n - desviacion_plastico),color='k',label = 'Desviacion estandar')
+ax.plot(rango,np.full_like(rango,promedio_n + desviacion_plastico),color='k',label = 'Desviacion estandar')
 ax.scatter([tuplas[0] for tuplas in tuplas_plastico],[tuplas[1] for tuplas in tuplas_plastico])
-
-#Promedio n
-resultado = 0
-for promedio in tuplas_plastico:
-    resultado += promedio[1]
-resultado = resultado/len(tuplas_plastico)
-print(resultado)
-
-#Desviacion estandar
-
-
-
+ax.legend()
 plt.savefig('Kapton.png')
 
+
+lista_elementos = pd.read_csv('./indices_refraccion.csv')
+
+#Funcion en general
+def funcion_general(lista_elementos:pd.DataFrame):
+
+    
+
+
+    for i in range(len(lista_elementos)):
+        arreglo_general = tuplas_longitud_onda(lista_elementos.iloc[i][0] + '/' + lista_elementos.iloc[i][3].split('/')[-1])
+        promedio_general = promedio(arreglo_general)
+        desviacion_estandar_general = desviacion_estandar(arreglo_general,promedio_general)
+        
+
+        #Graficar y guardar en cada uno
+        fig,ax = plt.subplots()
+        ax.set_title(lista_elementos.iloc[i][0])
+        rango = np.linspace(min([tuplas[0] for tuplas in arreglo_general]),max([tuplas[0] for tuplas in arreglo_general]),100)
+        ax.plot(rango,np.full_like(rango,promedio_general),color='r',label = 'promedio')
+        ax.plot(rango,np.full_like(rango,promedio_general - desviacion_estandar_general),color='k',label = 'Desviacion estandar')
+        ax.plot(rango,np.full_like(rango,promedio_general + desviacion_estandar_general),color='k',label = 'Desviacion estandar')
+        ax.scatter([tuplas[0] for tuplas in arreglo_general],[tuplas[1] for tuplas in arreglo_general])
+        ax.legend()
+        plt.savefig(lista_elementos.iloc[i][0] + '/' + lista_elementos.iloc[i][2] +'.png')
+        plt.close()
+
+funcion_general(lista_elementos=lista_elementos)       
+
+        
+
+       
+
+
+
+
+        
+
+
+   
