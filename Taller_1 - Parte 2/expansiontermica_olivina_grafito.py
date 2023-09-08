b99005d45f2c225b8111eb62dd12f07e54566054
Alejandri731 <j.cifuentesv@uniandes.edu.co>
2023-09-07 20:11:53 -0500

    Entrega final - Parte 2

--- Taller_1 - Parte 2/expansiontermica_olivina_grafito.py
+++ Taller_1 - Parte 2/expansiontermica_olivina_grafito.py
@@ -5,3 +5,8 @@ import numpy as np
 
 print(et.cohef_expansion(r"Taller_1 - Parte 2\graphite_mceligot_2016.csv"))
 print(et.cohef_expansion(r"Taller_1 - Parte 2\olivine_angel_2017.csv"))
+
+#coheficiente de expansión volumetrica
+#Grafito: 1.4   Error: 0.4
+#Olivine: 2.0   Error: 1.2
+
